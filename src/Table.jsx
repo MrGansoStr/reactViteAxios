f1aee5dd10a0f824fe5f5e29dd669713ba8b938f
MrGansoStr <ccounttnro1@gmail.com>
2022-11-04 17:48:12 -0500

    First Commit in reactViteAxios

---
+++ src/Table.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const Table = () => {
+
+  let [users, setusers] = useState([]);
+
+  const getUsers = async (e) => {
+    e.preventDefault()
+    try {
+      const { data } = await axios.get('https://restapinormal.vercel.app/users');
+      //console.log(data)
+      setusers(data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="container-lg">
+    <table className="table border rounded-3">
+      <thead>
+        <tr>
+          <td>#</td>
+          <td>User</td>
+          <td>Email</td>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+    <div className="container-sm">
+      <button type="button" className="btn btn-success" onClick={ getUsers }>Get List Users</button>
+    </div>
+    </div>
+  );
+};
+
+export default Table;
